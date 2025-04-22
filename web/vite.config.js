94159cd40caf1e8f10c41ee4f546942ede358fba
Eugene Allenov <e.allenov@gmail.com>
2025-04-22 20:35:06 +0300

    feat: setup vite config for restaking

--- web/vite.config.js
+++ web/vite.config.js
@@ -1,7 +1,24 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import federation from '@originjs/vite-plugin-federation'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
+  plugins: [
+    svelte(),
+    federation({
+      name: 'svelte_app',
+      filename: 'remoteEntry.js',
+      exposes: {
+        './App': './src/App.svelte'
+      },
+      shared: []
+    })
+  ],
+  build: {
+    target: 'esnext'
+  },
+  server: {
+    port: 5001,
+    cors: true
+  }
 })
